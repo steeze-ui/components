25caf24b33d0b09d2de565f461f3c4d6b8b9e41a
justinvoitel <mail@justinv.me>
2022-02-09 18:44:52 +0100

    fix: switch adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
-// import adapter from '@sveltejs/adapter-auto'
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-auto'
+// import adapter from '@sveltejs/adapter-node'
 import preprocess from 'svelte-preprocess'
 import WindiCSS from 'vite-plugin-windicss'
 
