1ca558fcb16a65fbda9bfe22b03735fbb3b97e11
justinvoitel <mail@justinv.me>
2022-02-13 02:13:21 +0100

    chore: add parts to core/parts index export

---
+++ src/lib/core/parts/index.ts
@@ -0,0 +1,6 @@
+export { default as Label } from './Label.svelte'
+export { default as Helper } from './Helper.svelte'
+export { default as Portal } from './Portal.svelte'
+export { default as Switch } from './Switch.svelte'
+export { default as FieldContainer } from './FieldContainer.svelte'
+export { default as Floating } from './Floating.svelte'
