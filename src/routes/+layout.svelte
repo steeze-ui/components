757f478b05d41cc1d687fbf2575f09b1ec63f2d6
justinvoitel <mail@justinv.me>
2022-10-06 14:07:37 +0200

    feat: add new core styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
+	import '$lib/core.css'
 	import '$lib/base.css'
 	import '$lib/normalize.css'
 	import ThemeProvider from '$lib/theme/ThemeProvider.svelte'
 	import Header from '$lib/_internal/header/Header.svelte'
 	import 'virtual:windi.css'
-	import './../markdown.css'
 </script>
 
 <ThemeProvider fromSystem />
